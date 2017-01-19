c8d698d54eb0710ecb71d3676a1315b7c01256dc
Manuela Ljubicic <manuela.ljubicic@gmail.com>
2017-01-19 22:03:50 +0100

    task 3 - done

---
+++ task3/RunAll_Task3b.m
@@ -0,0 +1,18 @@
+%(1+1)ES
+yp = ones(1, 100); %100 dimensions = N
+sigmaStopCondition = 10^-5;
+sigma = 1;
+randn('state', 7); 
+
+[yOffsprings, sigmaHistory, offspringFitnessHistory] = OnePlusOne_ES_Task3('calcFitness', yp, sigma, sigmaStopCondition);
+
+semilogy(offspringFitnessHistory,'Color', 'blue');
+hold on;
+semilogy(sigmaHistory,'Color', 'green');
+
+xlabel('Generations', 'FontSize', 12);
+ylabel('Fitness', 'FontSize', 12);
+title('1/5 Rule', 'FontSize', 12);
+legend('Fitness-1/5 Rule','\sigmaHistory');
+offspringFitnessHistory(end) %last entry --> best offspring
+
