b55c1ca8955a4e5185ee09f5e5c8005493213c8d
Sergii Maidanov <malicx7@gmail.com>
2017-01-22 22:53:20 +0100

    updated task 6

---
+++ task6/Aufgabe6_TestScript.m
@@ -0,0 +1,21 @@
+randn('state', 7);
+    
+my = 3; % Size of parent population
+lambda = 10; % Skript, s. 95 Size of offspring population, lambda >= my Skript, s. 96
+N = 30; % Skript, s. 95
+rho = 1; % Size of family (parents)
+sigmaMutation = 1;
+sigmaStop = 10^(-5);
+
+[iterations, yOptimalVector, fitnessHistory, sigmaHistory] = MyMyILaSiSelfAdaptationES(sigmaMutation, sigmaStop, N, my, lambda, rho, 'QuadraticSphereModel', 1500);
+iterations
+
+semilogy(1:iterations+1, fitnessHistory, '-', 'Color', 'blue', 'linewidth', 2);
+hold on;
+semilogy(1:iterations+1, sigmaHistory, '-', 'Color', 'green', 'linewidth', 2);
+
+ylabel('Fitness', 'FontSize', 12);
+xlabel('Generations', 'FontSize', 12);
+
+title('My/MyI LaSi SA-ES', 'FontSize', 12);
+legend('FitnessHistory','\sigmaHistory');
