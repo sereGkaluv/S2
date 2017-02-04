2c9a608269e69556b554344f3986253a9834b06e
benimurza <benimurza@yahoo.com>
2017-02-04 13:35:10 +0100

    task12: diagram, plot

--- task12/Linse.m
+++ task12/Linse.m
@@ -28,23 +28,61 @@ C = eye(n); %Identity Matrix = Einheitsmatrix
 tau = 1/sqrt(2*n); %n = search space; tau = constant 
 tau_c = 1 + n*(n+1)/(2*mu); % (L1)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% here starts generation loop
-while( sigma > sigma_stop ) % (L2)
+iter = 1;
+
+while( sigma > sigma_stop )
     for l=1:lambda
-        sigmaTilde(l) = sigma * exp(tau*randn()); % (L3)
-        sTilde(:, l) = (det(chol(C))^(-1/n)*chol(C)')*randn(n,1); % (L4)
-        xTilde(:, l) = x + sigmaTilde(l)*sTilde(:, l); % (L5)
-        fTilde(l) = f_ges(xTilde(:, l)); % (L6)
+        sigmaTilde(l) = sigma * exp(tau*randn());
+        sTilde(:, l) = (det(chol(C))^(-1/n)*chol(C)')*randn(n,1);
+        xTilde(:, l) = x + sigmaTilde(l)*sTilde(:, l);
+        fTilde(l) = f_ges(xTilde(:, l));
     end
-    [fsorted, r] = sort(fTilde, 'ascend'); % (L7)
-    x = 1/mu * sum(xTilde(:, r(1:mu)), 2); % (L8)
-    sigma = 1/mu * sum(sigmaTilde(r(1:mu))); % (L9)
-    SumSS = zeros(n, n); % (L10)
-    for m=1:mu; SumSS = SumSS + sTilde(:, r(m))*sTilde(:, r(m))'; end; % (L10)
-    C = (1-1/tau_c)*C + (1/tau_c) * (1/mu)*SumSS; % (L10)
-    C = 0.5*(C+C'); % ensure symmetry of C-matrix
-end % (L11)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% after termination, abs(x) returns the x-coordinates (thicknesses)
-% of the optimized lens geometry
+    
+    [fsorted, r] = sort(fTilde, 'ascend');
+    BestResWithCovariance(iter) = fsorted(1);
+    x = 1/mu * sum(xTilde(:, r(1:mu)), 2);
+    sigma = 1/mu * sum(sigmaTilde(r(1:mu)));
+    SumSS = zeros(n, n);
+    
+    for m = 1 : mu; 
+        SumSS = SumSS + sTilde(:, r(m))*sTilde(:, r(m))'; 
+    end;
+    
+    C = (1-1/tau_c)*C + (1/tau_c) * (1/mu)*SumSS;
+    C = 0.5*(C+C');
+    
+    iter = iter + 1;
+end
+
+%reinital
+iter = 1;
+sigma = 1;
+x = LensParms.x_init*ones(n,1);
+
+
+while( sigma > sigma_stop && iter < 2000) %second and condtion -> backup
+    for l=1:lambda
+        sigmaTilde(l) = sigma * exp(tau*randn());
+        sTilde(:, l) = randn(n,1); %without Covar.
+        xTilde(:, l) = x + sigmaTilde(l)*sTilde(:, l);
+        fTilde(l) = f_ges(xTilde(:, l));
+    end
+    
+    [fsorted, r] = sort(fTilde, 'ascend');
+    BestResWithoutCovariance(iter) = fsorted(1);
+    x = 1/mu * sum(xTilde(:, r(1:mu)), 2);
+    sigma = 1/mu * sum(sigmaTilde(r(1:mu)));
+    
+    iter = iter + 1;
+end
+
+
+semilogy(BestResWithoutCovariance, '-', 'Color', 'blue', 'linewidth', 2);
+hold on;
+semilogy(BestResWithCovariance, '-', 'Color', 'green', 'linewidth', 2);
+
+ylabel('Fitness', 'FontSize', 12);
+xlabel('Generations', 'FontSize', 12);
+
+title('CMSA-ES', 'FontSize', 14);
+legend('Without Covariance Matrix','With Covariance Matrix');
