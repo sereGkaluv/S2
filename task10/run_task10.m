9d998a513d57694e4b442b491634b56a90ebcbeb
benimurza <benimurza@yahoo.com>
2017-02-06 21:22:21 +0100

    Added working first part of task10 + graph

--- task10/run_task10.m
+++ task10/run_task10.m
@@ -1,16 +1,16 @@
 randn('state', 7);
     
 
-lambda = 10; % Skript, s. 95 Size of offspring population, lambda >= my Skript, s. 96
-N = 30; % Skript, s. 95
+lambda = 20; 
+N = 30; 
 rho = 1; % Size of family (parents)
 sigmaMutation = 1;
 sigmaStop = 10^(-5);
 my = ones(N,1)*10; % Size of parent population
 
-[iterations, yOptimalVector, fitnessHistory, sigmaHistory] = MyMyILaSiSelfAdaptationES(sigmaMutation, sigmaStop, N, my, lambda, rho, 'QuadraticSphereModel', 3000);
+[iterations, yOptimalVector, fitnessHistory, sigmaHistory] = MyMyILaSiSelfAdaptationESwithNoise(sigmaMutation, sigmaStop, N, my, lambda, rho, 'QuadraticSphereModelWithNoise', 3000);
 iterations
-fitnessHistory(size(fitnessHistory, 2))
+%fitnessHistory(size(fitnessHistory, 2))
 
 semilogy(1:iterations+1, fitnessHistory, '-', 'Color', 'blue', 'linewidth', 2);
 hold on;
@@ -19,5 +19,5 @@ semilogy(1:iterations+1, sigmaHistory, '-', 'Color', 'green', 'linewidth', 2);
 ylabel('Fitness', 'FontSize', 12);
 xlabel('Generations', 'FontSize', 12);
 
-title('My/MyI LaSi SA-ES', 'FontSize', 12);
+title('My/MyI LaSi SA-ES with noise', 'FontSize', 12);
 legend('FitnessHistory','\sigmaHistory');
